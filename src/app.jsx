f298ca6c962c823e4a20a2f947654edabbeed124
MinKyu <alsrb8442@naver.com>
2020-12-28 21:09:45 +0900

    back to the list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 import VideoList from './components/video_list/video_list';
 
+
+// 검색 하면 항상 selectedVideo가 null로 다시 바뀌면 된다.
+// 그럴려면 SearchH
 
 const App = ({youtube}) => {
 
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) =>{
+    setSelectedVideo(video);
+  }
 
   const search = query => {
     youtube
       .search(query) //
-      .then(videos => setVideos(videos));
+      .then(videos => {
+        setVideos(videos);
+        setSelectedVideo(null);
+      });
   }
 
 
@@ -23,8 +35,21 @@ const App = ({youtube}) => {
 
   return(
     <div className={styles.app}>
-      <SearchHeader onSearch={search} />
-      <VideoList videos={videos}/>
+      <SearchHeader onSearch={search}  />
+      <section className={styles.content} > 
+        {selectedVideo && 
+          <div className={styles.detail} >
+              <VideoDetail video={selectedVideo}/>
+          </div>
+        }
+        <div className={styles.list}>
+          <VideoList 
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid' }
+          />
+        </div>
+      </section>
     </div>
     
   )
--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -26,7 +26,13 @@ const SearchHeader = ({onSearch}) => {
 				<img className={styles.img} src="/images/logo.png" alt="logo"/>
 				<h1 className={styles.title} >Youtube</h1>
 			</div>
-			<input ref={inputRef} className={styles.input} type="search" placeholder="Search..." onKeyPress={onKeyPress} />
+			<input 
+				ref={inputRef} 
+				className={styles.input} 
+				type="search" 
+				placeholder="Search..." 
+				onKeyPress={onKeyPress} 
+			/>
 			<button className={styles.button} type="submit" onClick={onClick} >
 				<img 
 					className={styles.buttonImg} 
